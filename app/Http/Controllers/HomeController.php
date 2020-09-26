0206b883b7459a774c488e8748be5e04d9a02807
Muhammad Daffa' A.R <daffavcd@gmail.com>
2020-09-26 11:06:44 +0700

    tambah fungsi chache di HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Article;
+use Illuminate\Support\Facades\Cache;
+
 class HomeController extends Controller
 {
-    
+
     public function __invoke(Request $request)
     {
-        $temp = Article::all();
-        return view('home', ['article'=> $temp]);
+        $value = Cache::rememberForever('users', function () {
+            return Article::all();
+        });
+        return view('home', ['article' => $value]);
     }
-    
 }
