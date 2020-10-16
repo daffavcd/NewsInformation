34e4a1ec6354726cd9837e2c9ca8690b0247dcde
Muhammad Daffa' A.R <daffavcd@gmail.com>
2020-10-16 10:19:33 +0700

    tambah migration dan model comment

---
+++ database/migrations/2020_10_16_031142_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_article');
+            $table->integer('id_user');
+            $table->string('comment');
+            $table->integer('id_comment_parent');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
