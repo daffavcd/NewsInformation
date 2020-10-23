fb5a68cf9a3e427830c58af545c65ddf4ccb28dd
Muhammad Daffa' A.R <daffavcd@gmail.com>
2020-10-23 14:39:55 +0700

    edit delete child komen

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -154,15 +154,67 @@
       </div>
       <?php 
         }
+        
         ?>
       {{-- TAMPIL CHILD KOMEN --}}
+      @php
+      $total_child=1;
+      @endphp
       @foreach ($anak_comment[$index] as $item)
-
       <div class="media mt-4">
         <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
-        <div class="media-body">
-          <h5 class="mt-0">{{$item->name}}</h5>
-          {{$item->comment}}
+        <div class="media-body" style="display: grid;">
+          <div style="display: block">
+            <h5 class="mt-0" style="float: left">{{$item->name}}</h5>
+            <div class="btn-group" style="float:right;">
+              <button type="button" class="btn btn-primary btn-sm " data-toggle="dropdown" aria-haspopup="true"
+                aria-expanded="false">
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-three-dots-vertical" fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd"
+                    d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z">
+                  </path>
+                </svg>
+              </button>
+
+              <div class="dropdown-menu">
+                <?php
+                if($user->id==$item->id_user){
+                ?>
+                <a class="dropdown-item edit_{{$total}}_{{$total_child}}" href="#">Edit</a>
+                <a class="dropdown-item delet" onclick="delet({{$item->id_comment}})" href="#">Delete</a>
+                <?php
+              }
+              
+              ?>
+              </div>
+            </div>
+          </div>
+          <div id="komen_{{$total}}_{{$total_child}}">
+            {{$item->comment}}
+          </div>
+          <?php
+        if(auth()->check()){
+          if($user->id==$item->id_user){
+          ?>
+          {{-- UPDATE FORM KOMEN CHILD --}}
+          <div style="display: block;margin-top: 10px;">
+            <form method="POST" action="/updateComment">
+              @csrf
+              <input type="hidden" name="id" value="{{$item->id_comment}}">
+              <input class="form-control form-control-sm edit_komen_{{$total}}_{{$total_child}}"
+                style="width: 80%;display:none;float:left" type="text" name="comment" value="{{$item->comment}}">
+              <button type="submit" style="margin-left: 6px;display:none"
+                class="btn btn-sm btn-primary edit_komen_{{$total}}_{{$total_child}}">Save</button>
+              <button type="button" style="margin-left: 6px;display:none"
+                class="btn btn-sm btn-light cancel_komen_{{$total}}_{{$total_child}}">Cancel</button>
+            </form>
+          </div>
+          <?php
+    } 
+    }
+    $total_child+=1;
+    ?>
         </div>
       </div>
       @endforeach
@@ -276,7 +328,25 @@
 <script type="text/javascript">
   <?php
       for ($i=1; $i < $total; $i++) { 
+      for ($j=1; $j <= $i; $j++) { 
         ?>
+  $(".edit_<?php echo $i ?>_<?php echo $j ?>").click(function(evt)
+  {
+  $(".edit_komen_<?php echo $i ?>_<?php echo $j ?>").show("slow");
+  $(".cancel_komen_<?php echo $i ?>_<?php echo $j ?>").show("slow");
+  $("#komen_<?php echo $i ?>_<?php echo $j ?>").hide();
+  evt.preventDefault();
+  });
+  $(".cancel_komen_<?php echo $i ?>_<?php echo $j ?>").click(function(evt)
+  {
+  $(".edit_komen_<?php echo $i ?>_<?php echo $j ?>").hide();
+  $(".cancel_komen_<?php echo $i ?>_<?php echo $j ?>").hide();
+  $("#komen_<?php echo $i ?>_<?php echo $j ?>").show();
+  evt.preventDefault();
+  });
+  <?php 
+        }
+        ?>  
   $(".edit_<?php echo $i ?>").click(function(evt)
   {
   $(".edit_komen_<?php echo $i ?>").show("slow");
@@ -303,7 +373,9 @@
   $(".cancel_reply_komen_<?php echo $i ?>").hide();
   evt.preventDefault();
   });
-  <?php } ?>
+  <?php
+      }
+       ?>
   function
   delet(key,evt){
   $('#deletemodal').modal('show');
